9941da45573f5554298247c581733a033bedb23e
cwc0202 <107936539+cwc0202@users.noreply.github.com>
2023-01-27 10:34:21 -0600

    Finished classes per instructions

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -16,10 +16,30 @@ public class PositionType {
         this.value = value;
     }
 
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    // DTODO: Add a custom toString() method that returns the data stored in 'value'.
 
-    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    // DTODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PositionType that = (PositionType) o;
+
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 
     // Getters and Setters:
 
